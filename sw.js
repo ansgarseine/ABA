c85c5773465ee0c2c694582ff4e29c2a242eafa6
ansgarseine <ansgar.seine@googlemail.com>
2026-05-20 21:08:18 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "aba-cache-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./ABA_iOS_Final.html",
+  "./style.css",
+  "./main.js",
+  "./bilder/icon-192.png",
+  "./bilder/icon-512.png"
+  // Wenn du später weitere Dateien oder Bilder hinzufügst, einfach hier ergänzen.
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
