1f0ff573a676b9e7c265700d64f1f5be419b25c2
Ansgar <ansgar.seine@googlemail.com>
2026-05-23 12:25:01 +0200

    Treppenhaus‑Modul integriert + neue Icons

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const CACHE = "aba-cache-v1";
 
 const FILES = [
@@ -25,3 +26,32 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+=======
+const CACHE = "aba-cache-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./ABA_iOS_Final.html",
+  "./style.css",
+  "./main.js",
+  "./bilder/icon-192.png",
+  "./bilder/icon-512.png"
+  // Wenn du später weitere Dateien oder Bilder hinzufügst, einfach hier ergänzen.
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+>>>>>>> afd70df (Treppenhaus‑Modul integriert + neue Icons)
